a2714f7359fb18e0020425d89c8f40aa5fe801d4
Alexbarile <alexbarile93fg@yahoo.it>
2023-02-24 18:01:10 +0100

    modal_delete completo

---
+++ resources/views/comics/modal_delete.blade.php
@@ -0,0 +1,19 @@
+<div class="modal fade" id="delete-comic-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h2 class="modal-title" id="exampleModalLabel">ATTENZIONE</h2>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            
+          <!-- span vuoto e riempito con JS dopo -->
+         Sei sicuro di voler cancellare l'elemento selezionato? <span id="modal-item-title"></span> 
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
+          <button type="button" class="btn btn-primary" id="confirm-delete">Cancella</button>
+        </div>
+      </div>
+    </div>
+</div>
